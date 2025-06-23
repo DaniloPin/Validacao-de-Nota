fd7a865b3a2da09fea5d3dd5304fdb83593a644e
Danilo Gonçalves Pinheiro <124277720+DaniloPin@users.noreply.github.com>
2025-06-23 19:49:03 -0300

    Add files via upload

---
+++ Avaliacao.cs
@@ -0,0 +1,22 @@
+﻿public class Avaliacao 
+{
+    public string Aluno { get; set; }
+    public double Nota { get; private set; }
+
+    public Avaliacao(string aluno) {
+        Aluno = aluno;
+    }
+
+    public void AtribuirNota(double nota) { 
+        Nota = nota;
+
+        if (nota >= 0 && nota <= 10) {
+            Nota = nota;
+
+        }
+        else {
+            Console.WriteLine("Erro: A nota deve estar entre 0 e 10.");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Avaliacao avalicao = new Avaliacao("Débora Pinheiro");
+
+avalicao.AtribuirNota(11);
+avalicao.AtribuirNota(8.5);
+
+Console.WriteLine($"Aluna {avalicao.Aluno} recebeu a nota {avalicao.Nota}");
